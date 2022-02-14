a1e77bcdbb7150ab728241c5e3b7db1b24f222fa
R. Wigar Wijaya W <wigarwijaya@gmail.com>
2022-02-14 22:04:36 +0700

    version one finished

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-// const playerBatu = document.getElementById("player-batu");
-// const playerKertas = document.getElementById("player-kertas");
-// const playerGunting = document.getElementById("player-gunting");
-
 const comBatu = document.querySelector(".com-batu");
 const comKertas = document.querySelector(".com-kertas");
 const comGunting = document.querySelector(".com-gunting");
-
-// const playerWin = document.getElementById("player-win");
-// const comWin = document.getElementById("com-win");
-// const draw = document.getElementById("draw");
-
 
 // player pick highlight
 
@@ -32,7 +23,6 @@ hands.forEach((element) => {
 // computer random pick & highlight
 
 let computerPick = ""
-
 function getComPick() {
     const comPick = Math.floor(Math.random() * 3);
     console.log("random: ", comPick);
@@ -41,7 +31,6 @@ function getComPick() {
     if (comPick === 1) computerPick = 'kertas';
     if (comPick === 2) computerPick = 'gunting';
 };
-
 
 const comHands = document.querySelectorAll(".com")
 
@@ -59,8 +48,6 @@ function comHighlight() {
 // result function
 
 let showResult = ""
-
-
 function getResult(playerPick, comPick) {
     if (playerPick == comPick) return "draw";
     if (playerPick == "batu") return (comPick == "gunting") ? "playerWin" : "comWin";
@@ -82,8 +69,7 @@ function popResult(result) {
     };
 };
 
-
-
+// trigger
 
 const chooseBatu = document.querySelector(".batu");
 chooseBatu.addEventListener('click', function() {
@@ -98,7 +84,6 @@ chooseBatu.addEventListener('click', function() {
     console.log('comp : ' + computerTurn);
     console.log('hasil : ' + result);
 });
-
 
 const chooseKertas = document.querySelector(".kertas");
 chooseKertas.addEventListener('click', function() {
@@ -127,3 +112,5 @@ chooseGunting.addEventListener('click', function() {
     console.log('comp : ' + computerTurn);
     console.log('hasil : ' + result);
 });
+
+// end.....
