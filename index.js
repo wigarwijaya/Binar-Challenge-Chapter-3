c528d9e4fb682d43a5ebe61fd99edfd3b9db43de
R. Wigar Wijaya W <wigarwijaya@gmail.com>
2022-03-06 23:32:16 +0700

    routing

--- index.js
+++ index.js
@@ -1,3 +1,54 @@
-const os = require('os');
+const express = require("express");
+const app = express();
 
-console.log('Free Memory :', os.freemem());
+app.use(express.urlencoded());
+
+let isLogin = false;
+
+app.set("view engine", "ejs");
+
+app.use((req, res, next) => {
+  // isLogin = false;
+  // !isLogin = true;
+
+  if (req.url === 'game' && !isLogin) {
+    res.redirect('/login');
+  }
+
+  next();
+});
+
+// EJS HOME PAGE
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// EJS PAGE LOGIN
+app.get("/login", (req, res) => {
+  res.render("login", {
+    error: '',
+  });
+});
+
+
+// API LOGIN
+app.post("/login/auth", (req, res) => {
+  const user = require('./db/user.json');
+
+  if (user.email === req.body.uEmail && user.password === req.body.uPassword) {
+    isLogin = true;
+    res.redirect('/game');
+  } else {
+    res.render("login", {
+      error: 'Your password and email was wrong',
+    });
+  }
+});
+
+// EJS GAME PAGE
+app.get("/game", (req, res) => {
+    res.render("game");
+  });
+  
+
+app.listen(8080, () => console.log("Server Running ..."));
